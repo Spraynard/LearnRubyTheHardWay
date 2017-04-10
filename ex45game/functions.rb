69614ec0ce0e9ff9efd47970515549e6783491c2
Spraynard <kellan.martin@gmail.com>
2017-04-10 18:49:02 -0400

    added newlines to puts

--- ex45game/functions.rb
+++ ex45game/functions.rb
@@ -1,6 +1,6 @@
 def roll_die
 	roll = 1 + rand(0...20)
-	puts "You roll a 20 Sided Die, and the roll you got was #{roll}"
+	puts "\nYou roll a 20 Sided Die, and the roll you got was #{roll}\n"
 	return roll
 end
 
